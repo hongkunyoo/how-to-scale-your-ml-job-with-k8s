c1f6e95ecd54cfa0eca79cb0d1a21fce7c16b479
hongkunyoo <hongkunyoo@gmail.com>
2019-07-18 12:20:55 +0000

    a

--- hands-on/09-ml-pipeline/README.md
+++ hands-on/09-ml-pipeline/README.md
@@ -41,12 +41,28 @@ ACCESS_KEY=$ACCESS_KEY SECRET_KEY=$SECRET_KEY python 03_output.py $BUCKET_NAME, 
 ### pipeline.yaml 설명
 
 #### Volumes
-- wf-storage
-- model-storage
+- wf-storage: workflow 실행 시점에서 dynamic하게 생성되는 임시 저장소(학습 데이터 저장)
+- model-storage: 학습한 모델 저장소
 
 #### Templates
 - alarm-slack: workflow 종료시 slack으로 알람을 보내는 template
 - input: s3에서 데이터를 받아오는 template
 - train: 학습 template
 - output: 학습된 모델파일을 s3로 업로드하는 template
+
+
+```bash
+# ml pipeline 실행
+kubectl apply -f pipeline.yaml
+```
+
+
+### 동적으로 pipeline 제작 하기
+
+지금까지는 미리 workflow YAML 파일을 만들어서 워크플로우를 호출하였습니다. 이번에는 파이썬 스크립트를 이용하여 필요한 인자 및 모델 실험 개수를 원하는대로 늘릴 수 있게 만들어 봅시다.
+
+1. template (pipeline-template.yaml)을 python jinja2를 이용하여 불러옵니다.
+2. 학습에 필요한 인자들을 원하는 만큼 입력합니다.
+3. template rendering을 합니다 --> 완성된 argo workflow 생성
+4. 완성된 yaml 파일을 쿠버네티스 마스터에 전달합니다.
 
--- hands-on/09-ml-pipeline/build_pipeline.py
+++ hands-on/09-ml-pipeline/build_pipeline.py
@@ -3,21 +3,22 @@ import datetime
 import jinja2
 
 
+# Load pipeline-template.yaml
 templateLoader = jinja2.FileSystemLoader(searchpath="./")
 templateEnv = jinja2.Environment(loader=templateLoader)
 TEMPLATE_FILE = "pipeline-template.yaml"
 template = templateEnv.get_template(TEMPLATE_FILE)
 
 
+# Insert model experiments
 steps = []
-
 
 steps.append({
     "name": "train0101",
     "epoch": 1,
     "activate": "relu",
     "drouput": 0.2,
-    "data-path": "/root"
+    "data-path": "/wf_storage"
 })
 
 steps.append({
@@ -25,17 +26,18 @@ steps.append({
     "epoch": 2,
     "activate": "selu",
     "drouput": 0.3,
-    "data-path": "/root2"
+    "data-path": "/wf_storage"
 })
 
+# Render template
 idx = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-ren = template.render(wfIdx=idx, slack_url='abc', steps=steps)
-print(ren)
+rendered = template.render(wfIdx=idx, slack_url='abc', steps=steps)
 
 
+# Apply workflow
 cmd = """cat << EOF | kubectl apply -f -
 %s
 EOF
-""" % ren
+""" % rendered
 
 os.system(cmd)
